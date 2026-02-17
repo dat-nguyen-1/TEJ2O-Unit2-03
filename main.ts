b7c3b2837258783a125cfae716fdb46c52c989cc
dat-nguyen-1 <dat.nguyen@stu.ocsb.ca>
2026-02-17 13:22:49 -0500

    will show perimeter and area of rectangle

--- main.ts
+++ main.ts
@@ -5,9 +5,6 @@
  * This program will show the area and perimeter of a rectangle.
 */
 
-const length: number = 5
-const width: number = 3
-
-basic.showString(`A rectangle has dimensions ${length} cm & ${width} cm.`)
-basic.showString(`The perimeter would be: ${2 * (length + width)} cm.`)
-basic.showString(`The area would be: ${length * width} cm^2.`)
+basic.showString('A rectangle has dimensions 5 cm & 3 cm.')
+basic.showString('The perimeter would be: ' + (2 * (5 + 8)).toString() + ' cm.')
+basic.showString('The area would be: ' + (5 * 8).toString() + ' cm^2.')
