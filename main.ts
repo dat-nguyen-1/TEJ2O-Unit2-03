0039d40056fa408654afc46607d16c74746c7c51
dat-nguyen-1 <dat.nguyen@stu.ocsb.ca>
2026-02-17 13:14:31 -0500

    change length and width variable to constants

--- main.ts
+++ main.ts
@@ -5,8 +5,8 @@
  * This program will show the area and perimeter of a rectangle.
 */
 
-let length: number = 5
-let width: number = 3
+const length: number = 5
+const width: number = 3
 
 basic.showString(`A rectangle has dimensions ${length} cm & ${width} cm`)
 basic.showString(`The perimeter would be: ${2 * (length + width)} cm`)
