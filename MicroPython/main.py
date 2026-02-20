bd71d02fdd7a210edc92961adebdbf901d0ca53e
dat-nguyen-1 <dat.nguyen@stu.ocsb.ca>
2026-02-20 04:30:52 +0000

    update main.py and create hex file

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -1,9 +1,11 @@
 """
-Created by: Mr. Coxall
-Created on: Sep 2020
+Created by: Dat Nguyen
+Created on: Feb 2026
 This module is a Micro:bit MicroPython program
 """
 
 from microbit import *
 
-display.scroll("Hello, World!")
+display.scroll(f"A rectangle has dimensions 5 cm & 3 cm.")
+display.scroll(f"The perimeter would be: {2 * (5 + 3)} cm.")
+display.scroll(f"The area would be: {5 * 3} cm^2.")
